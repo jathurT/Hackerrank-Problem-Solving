01efdce66e9e4dec71ad9633de157bb3ab8810c9
jathurT <jathurshan_t_e23@engug.ruh.ac.lk>
2024-07-26 09:01:19 +0530

    Solution Updated

--- src/Number_Line_Jumps/Solution.java
+++ src/Number_Line_Jumps/Solution.java
@@ -17,7 +17,11 @@ class Result {
 
   public static String kangaroo(int x1, int v1, int x2, int v2) {
     // Write your code here
-
+    if (v1 > v2 && (x2 - x1) % (v1 - v2) == 0) {
+      return "YES";
+    } else {
+      return "NO";
+    }
   }
 
 }
