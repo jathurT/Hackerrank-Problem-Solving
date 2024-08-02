3342addbe3a66db085d19232eec93c51b9cabc30
jathurT <jathurshan_t_e23@engug.ruh.ac.lk>
2024-08-02 09:29:05 +0530

    Solution Updated

--- src/Bill_Division/Solution.java
+++ src/Bill_Division/Solution.java
@@ -21,9 +21,19 @@ class Result {
 
   public static void bonAppetit(List<Integer> bill, int k, int b) {
     // Write your code here
-
+    int sum = 0;
+    for (int i = 0; i < bill.size(); i++) {
+      if (i != k) {
+        sum += bill.get(i);
+      }
+    }
+    int annaShare = sum / 2;
+    if (annaShare == b) {
+      System.out.println("Bon Appetit");
+    } else {
+      System.out.println(b - annaShare);
+    }
   }
-
 }
 
 public class Solution {
