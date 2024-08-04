e66d127a399173794d64c09159e8a9038297bf8f
jathurT <jathurshan_t_e23@engug.ruh.ac.lk>
2024-08-04 08:07:01 +0530

    Solution Updated

---
+++ src/Drawing_Book/Solution.java
@@ -0,0 +1,41 @@
+package Drawing_Book;
+
+import java.io.*;
+
+class Result {
+
+  /*
+   * Complete the 'pageCount' function below.
+   *
+   * The function is expected to return an INTEGER.
+   * The function accepts following parameters:
+   *  1. INTEGER n
+   *  2. INTEGER p
+   */
+
+  public static int pageCount(int n, int p) {
+    // Write your code here
+    int front = p / 2;
+    int back = (n / 2) - (p / 2);
+    return Math.min(front, back);
+  }
+}
+
+public class Solution {
+  public static void main(String[] args) throws IOException {
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+    int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+    int p = Integer.parseInt(bufferedReader.readLine().trim());
+
+    int result = Result.pageCount(n, p);
+
+    bufferedWriter.write(String.valueOf(result));
+    bufferedWriter.newLine();
+
+    bufferedReader.close();
+    bufferedWriter.close();
+  }
+}
