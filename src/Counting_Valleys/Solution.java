f8892619607663df7fda6baca2351df91ddc2956
jathurT <jathurshan_t_e23@engug.ruh.ac.lk>
2024-08-05 20:11:54 +0530

    Solution Updated

---
+++ src/Counting_Valleys/Solution.java
@@ -0,0 +1,52 @@
+package Counting_Valleys;
+
+import java.io.*;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+class Result {
+
+  /*
+   * Complete the 'countingValleys' function below.
+   *
+   * The function is expected to return an INTEGER.
+   * The function accepts following parameters:
+   *  1. INTEGER steps
+   *  2. STRING path
+   */
+
+  public static int countingValleys(int steps, String path) {
+    // Write your code here
+    List<Character> characterList = path
+            .replaceAll("\\s+", "")
+            .chars()
+            .mapToObj(c -> (char) c)
+            .toList();
+    for (int i = 0; i < steps; i++) {
+      int numOfValley = 0;
+
+    }
+
+  }
+
+}
+
+public class Solution {
+  public static void main(String[] args) throws IOException {
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+    int steps = Integer.parseInt(bufferedReader.readLine().trim());
+
+    String path = bufferedReader.readLine();
+
+    int result = Result.countingValleys(steps, path);
+
+    bufferedWriter.write(String.valueOf(result));
+    bufferedWriter.newLine();
+
+    bufferedReader.close();
+    bufferedWriter.close();
+  }
+}
