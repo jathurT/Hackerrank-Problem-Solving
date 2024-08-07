fcb581d67dee14baa4a7d082be0e6b20cfe9f704
jathurT <jathurshan_t_e23@engug.ruh.ac.lk>
2024-08-07 13:20:07 +0530

    Solution Updated

---
+++ src/Cats_and_a_Mouse/Solution.java
@@ -0,0 +1,50 @@
+package Cats_and_a_Mouse;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Solution {
+
+  // Complete the catAndMouse function below.
+  static String catAndMouse(int x, int y, int z) {
+    int diffXtoZ = Math.abs(x - z);
+    int diffYtoZ = Math.abs(y - z);
+    if (diffYtoZ == diffXtoZ)
+      return "Mouse C";
+    else if (diffYtoZ > diffXtoZ)
+      return "Cat A";
+    else
+      return "Cat B";
+
+  }
+
+  private static final Scanner scanner = new Scanner(System.in);
+
+  public static void main(String[] args) throws IOException {
+    BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+    int q = scanner.nextInt();
+    scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+    for (int qItr = 0; qItr < q; qItr++) {
+      String[] xyz = scanner.nextLine().split(" ");
+
+      int x = Integer.parseInt(xyz[0]);
+
+      int y = Integer.parseInt(xyz[1]);
+
+      int z = Integer.parseInt(xyz[2]);
+
+      String result = catAndMouse(x, y, z);
+
+      bufferedWriter.write(result);
+      bufferedWriter.newLine();
+    }
+
+    bufferedWriter.close();
+
+    scanner.close();
+  }
+}
