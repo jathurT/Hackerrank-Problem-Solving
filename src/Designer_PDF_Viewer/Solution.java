70b0b5675145771097e5f0e353298d5bf53b3f35
jathurT <jathurshan_t_e23@engug.ruh.ac.lk>
2024-08-15 06:02:50 +0530

    Designer PDF Viewer  Updated

--- src/Designer_PDF_Viewer/Solution.java
+++ src/Designer_PDF_Viewer/Solution.java
@@ -1,25 +1,25 @@
-package The_Hurdle_Race;
+package Designer_PDF_Viewer;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
 
 class Result {
 
-  public static int hurdleRace(int k, List<Integer> height) {
+  public static int designerPdfViewer(List<Integer> h, String word) {
     // Write your code here
-    int dose = 0;
-    Optional<Integer> maxElement = height.stream().max(Integer::compareTo);
-    int i = maxElement.get() - k;
-    if (i <= 0) {
-      return 0;
-    } else {
-      return i;
+    int maxHeight = 0;
+    for (int i = 0; i < word.length(); i++) {
+      int height = h.get(word.charAt(i) - 'a');
+      maxHeight = Math.max(maxHeight, height);
     }
+    return maxHeight * word.length();
   }
+
 }
 
 public class Solution {
@@ -27,17 +27,13 @@ public class Solution {
     BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-    String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
-
-    int n = Integer.parseInt(firstMultipleInput[0]);
-
-    int k = Integer.parseInt(firstMultipleInput[1]);
-
-    List<Integer> height = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+    List<Integer> h = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
             .map(Integer::parseInt)
             .collect(toList());
 
-    int result = Result.hurdleRace(k, height);
+    String word = bufferedReader.readLine();
+
+    int result = Result.designerPdfViewer(h, word);
 
     bufferedWriter.write(String.valueOf(result));
     bufferedWriter.newLine();
