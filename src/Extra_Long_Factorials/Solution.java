30b128fff474af96db57d06d624ca5292672a826
jathurT <jathurshan_t_e23@engug.ruh.ac.lk>
2024-08-27 06:28:45 +0530

    Extra Long Factorials Solution Updated

---
+++ src/Extra_Long_Factorials/Solution.java
@@ -0,0 +1,33 @@
+package Extra_Long_Factorials;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+class Result {
+
+  /*
+   * Complete the 'extraLongFactorials' function below.
+   *
+   * The function accepts INTEGER n as parameter.
+   */
+
+  public static void extraLongFactorials(int n) {
+    // Write your code here
+
+  }
+
+}
+
+public class Solution {
+  public static void main(String[] args) throws IOException {
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+    Result.extraLongFactorials(n);
+
+    bufferedReader.close();
+  }
+}
